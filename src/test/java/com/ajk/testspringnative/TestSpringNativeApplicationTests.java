b34d956d2dcbc757dcb281b7acbb093bad4a83b7
huangkai11 <huangkai11@58.com>
2021-12-27 11:16:36 +0800

    init

---
+++ src/test/java/com/ajk/testspringnative/TestSpringNativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajk.testspringnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringNativeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
